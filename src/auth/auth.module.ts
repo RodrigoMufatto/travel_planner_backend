cb38e3902fe85333cc3948c6e1b4fb77275c8e41
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-03-15 11:43:34 -0400

    feat: create Auth module with JWT authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { jwtConstants } from './constants';
+import { PrismaAuthRepository } from './repositories/prisma-auth.repository';
+import { AuthRepository } from './repositories/auth.repository';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    PrismaService,
+    PrismaAuthRepository,
+    { provide: AuthRepository, useClass: PrismaAuthRepository },
+  ],
+})
+export class AuthModule {}
