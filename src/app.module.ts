9fc0dad44104a7fe4f252a70c556a8c296e8e77e
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-03-16 15:48:50 -0400

    feat: import TripModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TripModule } from './trip/trip.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, TripModule],
   controllers: [AppController],
   providers: [AppService],
 })
