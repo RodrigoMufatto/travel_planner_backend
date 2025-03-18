9ac9c5b2a29ed2721a5e98a9b8d99ee6a3d91cf3
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-03-18 19:11:17 -0400

    feat: integrate ActivityModule into the main AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { TripModule } from './trip/trip.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, TripModule],
+  imports: [PrismaModule, AuthModule, TripModule, ActivityModule],
   controllers: [AppController],
   providers: [AppService],
 })
