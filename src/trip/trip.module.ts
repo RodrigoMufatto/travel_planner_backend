c0ebbc11bcdd58ca059b7a7fd7a0070396ff4b38
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-03-18 21:49:13 -0400

    refactor: change the imports in trip module

--- src/trip/trip.module.ts
+++ src/trip/trip.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TripController } from './trip.controller';
-import { TripService } from './trip.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaTripRepository } from './infra/repositories/prisma-trip.repository';
 import { TripRepository } from './domain/repositories/trip.repository';
+import { TripService } from './application/trip.service';
+import { TripController } from './controllers/trip.controller';
 
 @Module({
   controllers: [TripController],
