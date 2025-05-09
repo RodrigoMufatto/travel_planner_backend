02afc93be28a63783e5e67c334db834efc0b93df
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-05-08 21:01:35 -0400

    feat: validate destination existence in ActivityService

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -4,8 +4,10 @@ import { ActivityService } from './application/activity.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaActivityRepository } from './infra/repositories/prisma-activity.repository';
 import { ActivityRepository } from './domain/repositories/activity.repository';
+import { RepositoriesModule } from 'src/shared/repositories/repositories.module';
 
 @Module({
+  imports: [RepositoriesModule],
   controllers: [ActivityController],
   providers: [
     ActivityService,
